b90d5ae0b227fd152c8a54f6ddd551cddac5ac39
PIERRICK <pierrickviret@gmail.com>
2023-06-08 20:34:01 +0200

    fin couleur bouton

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let silverColor = UIColor(red: 89, green: 77, blue: 70, alpha: 1)
+    let goldLefColor = UIColor(red: 209, green: 178, blue: 128, alpha: 1)
+    
     let acButton = UIButton(type: .system)
     let pointButton = UIButton(type: .system)
     let signButton = UIButton(type: .system)
@@ -31,6 +35,7 @@ class ViewController: UIViewController {
         calculate = Calculator(delegate: self)
         
         setupBoutons()
+        setupButtonsLayout()
     }
 
     // View actions
@@ -74,30 +79,44 @@ class ViewController: UIViewController {
         for title in numbersButtonsTitles {
             let button = UIButton(type: .system)
             button.setTitle(title, for: .normal)
+            setColorButton(button, background: silverColor, titre: .white)
             button.addTarget(self, action: #selector(tappedNumberButton(_:)), for: .touchUpInside)
             numbersButtons.append(button)
         }
         
         // declaration of operateurs
-        for title in numbersButtonsTitles {
+        for title in operateursButtonsTitles {
             let button = UIButton(type: .system)
             button.setTitle(title, for: .normal)
-            button.backgroundColor = UIColor(red: 89, green: 77, blue: 70, alpha: 1.0)
+            setColorButton(button, background: goldLefColor, titre: silverColor)
             button.addTarget(self, action: #selector(tappedOpperatorButton(_:)), for: .touchUpInside)
             operateursButtons.append(button)
         }
         
         pointButton.setTitle(".", for: .normal)
         pointButton.addTarget(self, action: #selector(tappedPointButton(_:)), for: .touchUpInside)
+        setColorButton(pointButton, background: silverColor, titre: .white)
         
         signButton.setTitle("+/-", for: .normal)
         signButton.addTarget(self, action: #selector(tappedChangeSign(_:)), for: .touchUpInside)
+        setColorButton(signButton, background: goldLefColor, titre: silverColor)
         
         egalButton.setTitle("=", for: .normal)
         egalButton.addTarget(self, action: #selector(tappedEqualButton(_:)), for: .touchUpInside)
+        setColorButton(egalButton, background: goldLefColor, titre: silverColor)
         
         acButton.setTitle("AC", for: .normal)
         acButton.addTarget(self, action: #selector(tappedACButton(_:)), for: .touchUpInside)
+        setColorButton(acButton, background: goldLefColor, titre: silverColor)
+    }
+    
+    private func setColorButton(_ boutton: UIButton, background: UIColor, titre: UIColor){
+        boutton.backgroundColor = background
+        boutton.setTitleColor(titre, for: .normal)
+    }
+    
+    private func setupButtonsLayout() {
+        
     }
 }
 
