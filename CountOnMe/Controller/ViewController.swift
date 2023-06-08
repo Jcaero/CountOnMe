f401f2049b46301a5e7a18c87cd041a2657df8b8
PIERRICK <pierrickviret@gmail.com>
2023-06-08 20:12:04 +0200

    suppression storyboard ajout bouton

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -9,8 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var numberButtons: [UIButton]!
-    @IBOutlet weak var acButton: UIButton!
+    let acButton = UIButton(type: .system)
+    let pointButton = UIButton(type: .system)
+    let signButton = UIButton(type: .system)
+    let egalButton = UIButton(type: .system)
+    
+    let numbersButtonsTitles = ["0", "1", "2","3", "4", "5","6", "7", "8", "9"]
+    var numbersButtons: [UIButton] = []
+    
+    let operateursButtonsTitles = ["+", "-", "×","÷"]
+    var operateursButtons: [UIButton] = []
+    
     @IBOutlet weak var textLbl: UILabel!
     
     private var calculate: Calculator!
@@ -20,40 +29,75 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         updateDisplay("0")
         calculate = Calculator(delegate: self)
+        
+        setupBoutons()
     }
 
     // View actions
-    @IBAction func tappedNumberButton(_ sender: UIButton) {
-        guard let numberText = sender.title(for: .normal) else {
+    @objc func tappedNumberButton(_ sender: UIButton) {
+        guard let numberText = sender.currentTitle else {
             return
         }
         calculate.numberHasBeenTapped(numberText)
     }
 
-    @IBAction func tappedOpperatorButton(_ sender: UIButton) {
+    @objc func tappedOpperatorButton(_ sender: UIButton) {
         guard let operatorText = sender.title(for: .normal) else {
             return
         }
         calculate.operatorHasBeenTapped(operatorText)
     }
 
-    @IBAction func tappedEqualButton(_ sender: UIButton) {
+    @objc func tappedEqualButton(_ sender: UIButton) {
         calculate.egalHasBeenTapped()
     }
 
-    @IBAction func tappedACButton(_ sender: UIButton) {
+    @objc func tappedACButton(_ sender: UIButton) {
         guard let clearText = sender.title(for: .normal) else {
             return
         }
         calculate.clearExpression(clearText)
     }
 
-    @IBAction func tappedChangeSign(_ sender: UIButton) {
+    @objc func tappedChangeSign(_ sender: UIButton) {
         calculate.changeSignTapped()
     }
 
-    @IBAction func tappedPointButton(_ sender: UIButton) {
+    @objc func tappedPointButton(_ sender: UIButton) {
         calculate.pointHasBeenTapped()
+    }
+    
+    // creation of bouttons
+    private func setupBoutons() {
+        
+        // déclaration of bouttons nombre
+        for title in numbersButtonsTitles {
+            let button = UIButton(type: .system)
+            button.setTitle(title, for: .normal)
+            button.addTarget(self, action: #selector(tappedNumberButton(_:)), for: .touchUpInside)
+            numbersButtons.append(button)
+        }
+        
+        // declaration of operateurs
+        for title in numbersButtonsTitles {
+            let button = UIButton(type: .system)
+            button.setTitle(title, for: .normal)
+            button.backgroundColor = UIColor(red: 89, green: 77, blue: 70, alpha: 1.0)
+            button.addTarget(self, action: #selector(tappedOpperatorButton(_:)), for: .touchUpInside)
+            operateursButtons.append(button)
+        }
+        
+        pointButton.setTitle(".", for: .normal)
+        pointButton.addTarget(self, action: #selector(tappedPointButton(_:)), for: .touchUpInside)
+        
+        signButton.setTitle("+/-", for: .normal)
+        signButton.addTarget(self, action: #selector(tappedChangeSign(_:)), for: .touchUpInside)
+        
+        egalButton.setTitle("=", for: .normal)
+        egalButton.addTarget(self, action: #selector(tappedEqualButton(_:)), for: .touchUpInside)
+        
+        acButton.setTitle("AC", for: .normal)
+        acButton.addTarget(self, action: #selector(tappedACButton(_:)), for: .touchUpInside)
     }
 }
 
